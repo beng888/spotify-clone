12417fef73082e4d6cc83bd03d1b9f3f41c80148
lawrence ardosa <lawrenceardosa@gmail.com>
2020-09-22 15:42:11 +0800

    Updates

---
+++ precache-manifest.7a0e2a0d06c81caff6efc13e4f2c0e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6c780a2207056eca97fdc6e2e05d8f",
+    "url": "/repo_name/index.html"
+  },
+  {
+    "revision": "786a939c6fd46675ab92",
+    "url": "/repo_name/static/css/main.4cbee62d.chunk.css"
+  },
+  {
+    "revision": "63935be92cdb6652c8c3",
+    "url": "/repo_name/static/js/2.7e3b6a3a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/repo_name/static/js/2.7e3b6a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786a939c6fd46675ab92",
+    "url": "/repo_name/static/js/main.9583cf8f.chunk.js"
+  },
+  {
+    "revision": "3e931546c1718351aa55",
+    "url": "/repo_name/static/js/runtime-main.fccc6e93.js"
+  }
+]);
